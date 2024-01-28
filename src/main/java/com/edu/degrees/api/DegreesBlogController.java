e23cb6f0908adc7f8aad8d4d531ba339add863d1
natanE-Cscc <nejigu@student.cscc.edu>
2024-01-28 10:59:26 -0500

    adding working location

--- src/main/java/com/edu/degrees/api/DegreesBlogController.java
+++ src/main/java/com/edu/degrees/api/DegreesBlogController.java
@@ -1,7 +1,9 @@
 package com.edu.degrees.api;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/menu/categories")
 public class DegreesBlogController {
+    @GetMapping
+    public ResponseEntity getDegrees() {
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity("degrees", headers, HttpStatus.OK);
+    }
     @PostMapping
-    public ResponseEntity createDegreesEntry(){
-        return new ResponseEntity(HttpStatus.CREATED);
+    public ResponseEntity createDegreesEntry() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "http://localhost/api/menu/categories");
+        return new ResponseEntity(null, headers, HttpStatus.CREATED);
     }
 }
