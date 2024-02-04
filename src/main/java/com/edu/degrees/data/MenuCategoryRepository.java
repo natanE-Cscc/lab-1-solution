f271fc8860a381d8d51b5db9d740b4b7fe1a87a7
natanE-Cscc <nejigu@student.cscc.edu>
2024-02-03 23:49:56 -0500

    working solutions for lab-3

--- src/main/java/com/edu/degrees/data/MenuCategoryRepository.java
+++ src/main/java/com/edu/degrees/data/MenuCategoryRepository.java
@@ -3,5 +3,6 @@ package com.edu.degrees.data;
 import com.edu.degrees.domain.MenuCategory;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DegreesBlogRepository extends CrudRepository<MenuCategory, Long>{
+public interface MenuCategoryRepository extends CrudRepository<MenuCategory, Long> {
+
 }
